db5de86009047e2c0cca93877cc21a28e06467e9
Tycon_Xon <66801982+TyconXon@users.noreply.github.com>
2025-08-21 12:07:07 -0700

    did stuff?

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,6 +5,8 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * speed * delta
 	move_and_slide()
 	
+	$Arm.look_at(get_global_mouse_position())
+	
 	# Debug:
 	if Input.is_action_just_pressed("ui_accept"):
 		health += 1
