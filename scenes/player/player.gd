44595307a7d48138a3080c13eac66a8b07c58678
Tycon_Xon <66801982+TyconXon@users.noreply.github.com>
2025-08-22 11:19:22 -0700

    attacking it rn

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,24 @@
 extends Character
 
+@export var validSwordAngleStep = 15.0
+
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
-	velocity = direction * speed * delta
-	move_and_slide()
 	
-	$Arm.look_at(get_global_mouse_position())
+	# Can't do this stuff whilst attacking
+	if not $Arm/Attack.is_playing():
+		# Handle movement
+		var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+		velocity = direction * speed * delta
 	
+		# Rotate the sword toward the mouse
+		var angleToTheMouse = get_global_mouse_position().angle_to_point(position)
+		$Arm.rotation = deg_to_rad( snapped( rad_to_deg(angleToTheMouse) + 180 , validSwordAngleStep) )
+	else:
+		velocity = Vector2.ZERO
+		
 	# Debug:
 	if Input.is_action_just_pressed("ui_accept"):
-		health += 1
+		$Arm/Attack.play("jab")
+
+
+	move_and_slide()
