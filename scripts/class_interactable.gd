0fd08835e4778eac9df8e7cf332324bfd30b6e9f
Dinoman <126414593+dinoman009@users.noreply.github.com>
2025-08-28 14:55:47 -0700

    Big progress on interactables

--- scripts/class_interactable.gd
+++ scripts/class_interactable.gd
@@ -4,10 +4,27 @@ extends Area2D
 signal onPowerStateChanged
 signal onTouchingPlayer
 
-@export var locked : bool
-@export var powered : bool
+# Adds dropdown in editor for whether or not you want the interactable to be powered or locked by default
+@export_enum("true", "false") var initial_locked : String
+@export_enum("true", "false") var initial_powered : String
+
+var locked : bool
+var powered : bool
 
 
 
-func trigger():
+func _ready() -> void:
+	# Sets the powered and locked properties upon initializing
+	if initial_powered == "true":
+		powered = true
+	else:
+		powered = false
+
+	if initial_locked == "true":
+		locked = true
+	else:
+		locked = false
+
+func trigger() -> void:
+	#For when the player interacts with the interactables
 	print("I'm interacting it so good!!!")
