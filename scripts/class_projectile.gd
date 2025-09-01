05c3645c23c8fa1dde410acdd2535abacac594bc
Tycon_Xon <66801982+TyconXon@users.noreply.github.com>
2025-08-31 20:50:38 -0700

    Some more work on projectiles

--- scripts/class_projectile.gd
+++ scripts/class_projectile.gd
@@ -7,8 +7,10 @@ extends CharacterBody2D
 ## This uses CharacterBody2D, which seems counter-intuitive, but in reality is
 ## helpful for more reliable collision detection and movement.
 
+## I'm sorry this script looks awful and is poorly documented.
+
 @export var damage : int = 1
-@export var speed := 350
+@export var speed := 350.0
 @export var isFire := false
 
 @export_subgroup("Linked nodes")
@@ -18,6 +20,7 @@ extends CharacterBody2D
 
 @onready var bushTileMap : TileMapLayer = get_tree().get_first_node_in_group("BushLayer")
 
+@onready var _speed := speed 
 var lifetime := 0.0:
 	set(val):
 		if val >= MAXLIFETIME:
@@ -26,12 +29,16 @@ var lifetime := 0.0:
 		
 		# fade out sprite
 		#sprite.modulate.a = 1 - lifetime/MAXLIFETIME
+		if(speed != 0.0):
+			_speed = speed - (speed * (lifetime/MAXLIFETIME))
+		else:
+			_speed = max(_speed - 5.0,0)
 const MAXLIFETIME := 1.0
 
 ## Initial velocity of the projectile.
 ## For reference, think of how tears move while you move in tboi,
 ## They tilt if you move. Behavior is replicated here, just a bit extreme.
-var initial_velocity = Vector2.ZERO
+var initial_velocity := Vector2.ZERO
 
 #region Godot functions:
 
@@ -42,13 +49,16 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	# Move forward based on rotation.
-	velocity = initial_velocity + (transform.x * speed)
+	velocity = initial_velocity + (transform.x * _speed)
 	# Face the direction of motion.
 	if not isFire:
-		sprite.global_rotation = deg_to_rad(45.0) + velocity.angle()
-		collisionShape.global_rotation = sprite.global_rotation
+		if velocity:
+			sprite.global_rotation = deg_to_rad(45.0) + velocity.angle()
+			collisionShape.global_rotation = sprite.global_rotation
 	elif isFire:
 		lifetime += delta
+		if snappedf(lifetime,0.05) == snappedf(lifetime,0.1):
+			sprite.flip_h = !sprite.flip_h
 		sprite.global_rotation = 0.0
 		breakBushes()
 	
@@ -61,6 +71,10 @@ func _physics_process(delta: float) -> void:
 
 func on_touch_body(body: Node2D):
 	print(body)
+	speed = 0.0
+	if not isFire:
+		_speed = 0.0
+	initial_velocity = Vector2.ZERO
 	pass
 #endregion
 #region Custom Methods:
@@ -70,4 +84,5 @@ func breakBushes():
 	if bushTileMap.get_cell_tile_data(stoodOnTile) and\
 	bushTileMap.get_cell_tile_data(stoodOnTile).get_custom_data("bush"):
 		bushTileMap.erase_cell(stoodOnTile)
+		
 #endregion
