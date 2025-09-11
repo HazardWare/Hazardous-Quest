976276f0e93e0ad87d94a8156956287cc79e48c8
Dinoman <126414593+dinoman009@users.noreply.github.com>
2025-09-11 14:37:17 -0700

    BAT

---
+++ scenes/enemy/bat.gd
@@ -0,0 +1,40 @@
+extends Enemy
+
+@export var moving := false
+
+
+
+func _ready() -> void:
+	super()
+	fly()
+
+func _physics_process(delta: float) -> void:
+	characterProcess(delta)
+	
+	$EnemyComponents/HealthBar.value = health
+	$EnemyComponents/HealthBar.max_value = maximumHealth
+	
+	if $EnemyComponents/StallTimer.time_left != 0.0:
+		return
+	
+	move(delta)
+	continousDamage(delta)
+
+func fly():
+	$FlyTime.start(1)
+
+func move(delta):
+	if moving:
+		move_to(get_tree().get_first_node_in_group("Player").position, delta)
+	else:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		handlePush()
+
+func _on_fly_time_timeout() -> void:
+	if moving:
+		moving = false
+		fly()
+	else:
+		moving = true
+		fly()
