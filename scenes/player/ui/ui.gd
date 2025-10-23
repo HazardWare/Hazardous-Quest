80bbe6f76ba54b4d47db0666eece975f694513ee
WigglyStuf <SirCaptain35@outlook.com>
2025-10-23 12:14:19 -0700

    ackgub

--- scenes/player/ui/ui.gd
+++ scenes/player/ui/ui.gd
@@ -1,15 +1,27 @@
 extends Control
 
+signal signal_next
+
 @export var player : Character
 
 @onready var health_bar: HBoxContainer = $PanelTop/VBoxContainer/HealthBar
 @onready var blue_bar: HBoxContainer = $PanelTop/VBoxContainer/BlueBar
 @onready var voice_audio: AudioStreamPlayer = $DialoguePanel/VoiceAudio
 
-@onready var default_voice : VoiceResource = preload("res://resources/voice/voice_npc.tres")
+@onready var voice_princess : VoiceResource = preload("res://resources/voice/voice_princess.tres")
+@onready var voice_npc : VoiceResource = preload("res://resources/voice/voice_npc.tres")
+@onready var voice_sign : VoiceResource = preload("res://resources/voice/voice_sign.tres")
+@onready var voice_monster : VoiceResource = preload("res://resources/voice/voice_monster.tres")
+@onready var voice_thaddeon : VoiceResource = preload("res://resources/voice/voice_thaddeon.tres")
+@onready var voice_calamin : VoiceResource = preload("res://resources/voice/voice_calamin.tres")
+@onready var voice_ui : VoiceResource = preload("res://resources/voice/voice_ui.tres")
+
+@onready var box_name: Label = $DialoguePanel/VBoxContainer/Name
+
 
 func _ready() -> void:
 	Console.add_command("hq_say", dialogue_box_say, 1)
+
 
 func update_health():
 	
@@ -53,6 +65,12 @@ func update_health():
 
 		blue_bar.add_child(this_heart)
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if $DialoguePanel.visibile == true:
+		if Input.is_anything_pressed():
+			signal_next.emit()
+
 func _process(_delta: float) -> void:
 	if player.attackMode == "big_swipe":
 		$Action/Label.text = "SWIPE"
@@ -60,35 +78,44 @@ func _process(_delta: float) -> void:
 		$Action/Label.text = "JAB"
 
 
-func dialogue_box_say(dialogue : String, voice : VoiceResource = default_voice):
+func dialogue_box_say(dialogue : String, voice : VoiceResource = voice_ui):
+	$DialoguePanel.visibility = true
+	
+	box_name.visible = false if voice.speaker_name == "" else true
+	
 	$DialoguePanel/VBoxContainer/Speech.text = ""
 	$DialoguePanel/VBoxContainer/Name.text = voice.speaker_name
 	voice_audio.stream = voice.sound
+	voice_audio.volume_linear = voice.volume
+	
 	var speed : float = voice.speed
 	var this_speed : float = 0.0 # Speed for individual characters.
 	var mute : bool = false
+	
 	for i in len(dialogue):
+
 		this_speed = speed
-		
-		match dialogue[i]: # Speeds & Mutes
-			".":
-				this_speed *= 6
-				mute = true
-			"!":
-				this_speed *= 5.5
-				mute = true
-			"?":
-				this_speed *= 7
-				mute = true
-			":" , ";", ",":
-				this_speed *= 2
-				mute = true
-			"(", ")", "-":
-				this_speed *= 0
-				mute = true
-			_:
-				mute = false
-			
+
+		if voice != voice_sign:
+			match dialogue[i]: # Speeds & Mutes
+				".":
+					this_speed *= 6
+					mute = true
+				"!":
+					this_speed *= 5.5
+				"?":
+					this_speed *= 7
+					mute = true
+				":" , ";", ",":
+					this_speed *= 2
+					mute = true
+				"(", ")", "-":
+					this_speed *= 0
+					mute = true
+				_:
+					mute = false
+
 		$DialoguePanel/VBoxContainer/Speech.text += dialogue[i]
 		if !mute: voice_audio.play()
 		await get_tree().create_timer(this_speed).timeout
+	await signal_next
--- scripts/resources/voice.gd
+++ scripts/resources/voice.gd
@@ -3,3 +3,4 @@ class_name VoiceResource extends Resource
 @export var speaker_name : String
 @export_range(0.001,0.25,0.001) var speed : float ## Seconds between each character.
 @export var sound : AudioStream
+@export_range(0.0,1.0,0.1) var volume : float = 1.0
