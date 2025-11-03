e147f6a7e76d7de1e0291cf63cccbbf6e3b79615
Tycon_Xon <66801982+TyconXon@users.noreply.github.com>
2025-11-03 11:47:58 -0700

    work

--- scenes/components/playerDetection.gd
+++ scenes/components/playerDetection.gd
@@ -4,9 +4,10 @@ extends RayCast2D
 
 func _process(delta: float) -> void:
 	target_position = to_local(playerReference.global_position)
+	
 	if get_collider() and get_collider().get_parent() == playerReference:
 		get_parent().canSeePlayer = true
 		$"../OffsetDisconnect/LastSeenPosition".global_position = playerReference.global_position
 	else:
-		print(get_collider())
+		#print(get_collider())
 		get_parent().canSeePlayer = false
