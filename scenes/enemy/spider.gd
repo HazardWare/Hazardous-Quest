9cbef9cb7aee7ae82d31365d4ae3b63828d5ef0b
Dinoman <126414593+dinoman009@users.noreply.github.com>
2025-11-18 12:03:45 -0700

    Big progress on spider movement

--- scenes/enemy/spider.gd
+++ scenes/enemy/spider.gd
@@ -7,6 +7,8 @@ var direction : Vector2
 func _ready() -> void:
 	super()
 	jump()
+	calculateDirection()
+	
 
 func _physics_process(delta: float) -> void:
 	characterProcess(delta)
@@ -14,14 +16,14 @@ func _physics_process(delta: float) -> void:
 	$CollisionShape2D.position = $AnimatedSprite2D.position
 	if $EnemyComponents/StallTimer.time_left != 0.0:
 		return
-	
+		
 	move(delta)
 	continousDamage(delta)
 
 	
 
 func jump():
-	direction = Vector2(randi_range(-1,1), randi_range(-1,1))
+	calculateDirection()
 	$AnimationPlayer.play("jump")
 	await !moving
 	$WalkTimer.start(1)
@@ -29,7 +31,6 @@ func jump():
 func move(delta):
 	if moving:
 		velocity = speed * direction * delta
-		print(velocity)
 		move_and_slide()
 		handlePush()
 
@@ -37,15 +38,49 @@ func _on_walk_timer_timeout() -> void:
 	jump()
 
 func calculateDirection() -> void:
-	var x1
-	var x2
-	var y1
-	var y2
+	#Calculates how big one quarter of thr screen is (quadOut) and how big the "Safe Zone" is (quadIn) -A
+	var xQuadOut : float = (get_viewport_rect().size.x / get_canvas_transform().x.x) / 2
+	var xQuadIn : float = xQuadOut * 1/10
+	var yQuadOut : float = (get_viewport_rect().size.y / get_canvas_transform().y.y) / 2
+	var yQuadIn : float = yQuadOut * 0.2
 	
-	var xDif = playerReference.global_position.x - global_position.x
-	var yDif = playerReference.global_position.y - global_position.y
+	var quadIn : Vector2 = Vector2(xQuadIn, yQuadIn)
+	var quadOut : Vector2 = Vector2(xQuadOut, yQuadOut)
 	
-	if xDif > (get_viewport_rect().size.x - 160):
-		pass
-	elif xDif < (get_viewport_rect().size.x - 160):
-		pass
+	#Calculates Quadrant -A
+	var dist : Vector2 = global_position - playerReference.global_position
+	var quadrant : int
+	
+	if dist.x < 0 and dist.y < 0:
+		quadrant = 1 
+	elif dist.x > 0 and dist.y < 0:
+		quadrant = 2
+	elif dist.x < 0 and dist.y > 0:
+		quadrant = 3
+	elif dist.x > 0 and dist.y > 0:
+		quadrant = 4
+	else:
+		return
+	
+	#Calculates whether the spider is in the "Safe Zone" -A
+	var safe : bool
+	if abs(dist) >= quadIn:
+		safe = false
+	else:
+		safe = true
+	
+	#Calculated Direction
+	if safe:
+		direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	else:
+		if quadrant == 1:
+			direction = Vector2(randf_range(0, 1), randf_range(0, 1))
+		if quadrant == 2:
+			direction = Vector2(randf_range(-1, 0), randf_range(0, 1))
+		if quadrant == 3:
+			direction = Vector2(randf_range(0, 1), randf_range(-1, 0))
+		else:
+			direction = Vector2(randf_range(-1, 0), randf_range(-1, 0))
+	
+	print(safe)
+#Todo: Fix spider jumping away
