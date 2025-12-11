34cd76d6f52bed4d4fd323f4ef62d2ba5695d7b5
Dinoman <126414593+dinoman009@users.noreply.github.com>
2025-12-11 11:25:06 -0700

    door

--- scripts/inheritsInteractable/door.gd
+++ scripts/inheritsInteractable/door.gd
@@ -1,5 +1,31 @@
 extends Interactable
+class_name Door
 
-func _process(_delta: float) -> void:
-	super(_delta)
+@export var one : bool # Single door if true, double door if false
+@export var open : bool
+
+
+
+func _ready() -> void:
+	if one:
+		$InteractableArea.scale.x = 1
+		$StaticBody2D/CollisionShape2D.scale.x = 1
+		$Sprite2D.animation = "one"
+	else:
+		$InteractableArea.scale.x = 2.6
+		$StaticBody2D/CollisionShape2D.scale.x = 2.6
+		$Sprite2D.animation = "two"
+	
+	if open:
+		$Sprite2D.frame = 2
+	else:
+		$Sprite2D.frame = 0
+
+func _process(delta: float) -> void:
+	super(delta)
 	print(Global.inventory.bsearch("Key", true))
+	
+	if open:
+		$StaticBody2D/CollisionShape2D.disabled = true
+	else:
+		$StaticBody2D/CollisionShape2D.disabled = false
